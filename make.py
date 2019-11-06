26a515c4b24620ee6b5d7b8347f4cb15b702fb9d
Nicholas Frechette <zeno490@gmail.com>
2019-11-06 18:16:12 -0500

    feat: add support for GCC9 and clang7

--- make.py
+++ make.py
@@ -15,7 +15,7 @@ def parse_argv():
 	actions.add_argument('-unit_test', action='store_true')
 
 	target = parser.add_argument_group(title='Target')
-	target.add_argument('-compiler', choices=['vs2015', 'vs2017', 'vs2019', 'android', 'clang4', 'clang5', 'clang6', 'gcc5', 'gcc6', 'gcc7', 'gcc8', 'osx', 'ios'], help='Defaults to the host system\'s default compiler')
+	target.add_argument('-compiler', choices=['vs2015', 'vs2017', 'vs2019', 'android', 'clang4', 'clang5', 'clang6', 'clang7', 'gcc5', 'gcc6', 'gcc7', 'gcc8', 'gcc9', 'osx', 'ios'], help='Defaults to the host system\'s default compiler')
 	target.add_argument('-config', choices=['Debug', 'Release'], type=str.capitalize)
 	target.add_argument('-cpu', choices=['x86', 'x64', 'arm64'], help='Only supported for Windows, OS X, and Linux; defaults to the host system\'s architecture')
 
@@ -135,6 +135,9 @@ def set_compiler_env(compiler, args):
 		elif compiler == 'clang6':
 			os.environ['CC'] = 'clang-6.0'
 			os.environ['CXX'] = 'clang++-6.0'
+		elif compiler == 'clang7':
+			os.environ['CC'] = 'clang-7'
+			os.environ['CXX'] = 'clang++-7'
 		elif compiler == 'gcc5':
 			os.environ['CC'] = 'gcc-5'
 			os.environ['CXX'] = 'g++-5'
@@ -147,6 +150,9 @@ def set_compiler_env(compiler, args):
 		elif compiler == 'gcc8':
 			os.environ['CC'] = 'gcc-8'
 			os.environ['CXX'] = 'g++-8'
+		elif compiler == 'gcc9':
+			os.environ['CC'] = 'gcc-9'
+			os.environ['CXX'] = 'g++-9'
 		else:
 			print('Unknown compiler: {}'.format(compiler))
 			sys.exit(1)
