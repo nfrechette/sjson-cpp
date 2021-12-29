3bd8ad478c8264025acd0f3472e01744297f61af
Nicholas Frechette <zeno490@gmail.com>
2021-12-29 16:34:39 -0500

    feat: add support for native linux arm64 compilation

--- make.py
+++ make.py
@@ -36,6 +36,10 @@ def parse_argv():
 
 	args = parser.parse_args()
 
+	is_arm64_cpu = False
+	if platform.machine() == 'arm64' or platform.machine() == 'aarch64':
+		is_arm64_cpu = True
+
 	# Sanitize and validate our options
 	if args.compiler == 'android':
 		if not args.cpu:
@@ -76,20 +80,26 @@ def parse_argv():
 			sys.exit(1)
 	else:
 		if not args.cpu:
-			if platform.system() == 'Darwin' and platform.processor() == 'arm':
+			if is_arm64_cpu:
 				args.cpu = 'arm64'
 			else:
 				args.cpu = 'x64'
 
 	if args.cpu == 'arm64':
 		is_arm_supported = False
+
+		# Cross compilation
 		if args.compiler in ['vs2017', 'vs2019', 'ios', 'android']:
 			is_arm_supported = True
-		if platform.system() == 'Darwin' and platform.processor() == 'arm':
+
+		# Native compilation
+		if platform.system() == 'Darwin' and is_arm64_cpu:
+			is_arm_supported = True
+		elif platform.system() == 'Linux' and is_arm64_cpu:
 			is_arm_supported = True
 
 		if not is_arm_supported:
-			print('arm64 is only supported with VS2017, VS2019, Android, OS X (M1 processors), and iOS')
+			print('arm64 is only supported with VS2017, VS2019, OS X (M1 processors), Linux, Android, and iOS')
 			sys.exit(1)
 	elif args.cpu == 'armv7':
 		if not args.compiler == 'android':
